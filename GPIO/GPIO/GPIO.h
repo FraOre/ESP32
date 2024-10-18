bb5280788aed1074395a196c29c45c9709dc3d71
Francesco Orefice <francesco.orefice@immobiliare.it>
2024-10-18 19:23:36 +0200

    feat: first gpio component release

---
+++ GPIO/GPIO/GPIO.h
@@ -0,0 +1,43 @@
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+// Standard C++
+#include <functional>
+
+// FreeRTOS
+#include <freertos/FreeRTOS.h>
+
+// ESP-IDF
+#include <driver/gpio.h>
+
+enum class GPIODirection {
+    INPUT = 1,
+    OUTPUT
+};
+
+enum class GPIOInterruptType {
+    DISABLED,
+    NEGATIVE_EDGE,
+    POSITIVE_EDGE,
+    ANY_EDGE
+};
+
+class GPIO final {
+    public:
+        explicit GPIO(uint8_t pin, GPIODirection direction = GPIODirection::INPUT);
+        bool read() const;
+        void high() const;
+        void low() const;
+        void interruptEnable() const;
+        void interruptDisable() const;
+        void setInterruptType(GPIOInterruptType interruptType) const;
+        void addISRHandler(const std::function<void()>& handler);
+
+    private:
+        gpio_num_t _pin;
+        std::function<void()> _isrHandler;
+        static bool _isrServiceInstalled;
+        static void IRAM_ATTR interruptHandler(void* argument);
+};
+
+#endif
