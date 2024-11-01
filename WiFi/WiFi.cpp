38b748ddefedea605d4c9ac48443d61f464f6ed6
FraOre <orefice.fran@gmail.com>
2024-11-01 14:30:13 +0100

    chore: wifi component improvement to handle multiple modes and list clients and access points

--- WiFi/WiFi.cpp
+++ WiFi/WiFi.cpp
@@ -1,14 +1,7 @@
 #include "WiFi.h"
 
 WiFi::WiFi()
-    : _apConfig(), _staConfig(), _isConnected(false), _isConnecting(false), _manuallyDisconnect(false), _wiFiSTAEventGroup(nullptr) {}
-
-WiFi::~WiFi()
-{
-    vEventGroupDelete(_wiFiSTAEventGroup);
-}
-
-void WiFi::init()
+    : _apConfig(), _staConfig(), _netifAP(nullptr), _netifSta(nullptr), _isConnected(false), _isConnecting(false), _isManuallyDisconnect(false), _isAPStarted(false), _isAPStarting(false), _isSwitchingMode(false), _wiFiSTAEventGroup(nullptr)
 {
     if (nvs_flash_init() != ESP_OK) {
         nvs_flash_erase();
@@ -18,8 +11,19 @@ void WiFi::init()
     esp_event_loop_create_default();
     const wifi_init_config_t wiFiInitConfig = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&wiFiInitConfig);
+    esp_wifi_set_mode(WIFI_MODE_NULL);
+    esp_wifi_set_storage(WIFI_STORAGE_RAM);
     esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, &ipEventsHandler, this, nullptr);
     esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wiFiEventsHandler, this, nullptr);
+}
+
+WiFi::~WiFi()
+{
+    vEventGroupDelete(_wiFiSTAEventGroup);
+    esp_wifi_stop();
+    esp_wifi_deinit();
+    esp_event_loop_delete_default();
+    esp_wifi_set_mode(WIFI_MODE_NULL);
 }
 
 void WiFi::ipEventsHandler(void* arguments, const esp_event_base_t eventBase, const int32_t eventId, void* eventData)
@@ -28,6 +32,7 @@ void WiFi::ipEventsHandler(void* arguments, const esp_event_base_t eventBase, co
     if (eventId == IP_EVENT_STA_GOT_IP) {
         wiFi->_isConnected = true;
         wiFi->_isConnecting = false;
+        wiFi->_isManuallyDisconnect = false;
         xEventGroupSetBits(wiFi->_wiFiSTAEventGroup, WIFI_STA_CONNECTED_BIT);
         const auto* event = static_cast<ip_event_got_ip_t*>(eventData);
         char ip[IP4ADDR_STRLEN_MAX];
@@ -55,15 +60,18 @@ void WiFi::wiFiEventsHandler(void* arguments, esp_event_base_t eventBase, const 
         esp_wifi_connect();
     }
     else if (eventId == WIFI_EVENT_STA_DISCONNECTED) {
-        if (wiFi->_isConnecting) {
+        if (wiFi->_isSwitchingMode) {
+            wiFi->_isSwitchingMode = false;
+        }
+        else if (wiFi->_isConnecting) {
             wiFi->_isConnecting = false;
             xEventGroupSetBits(wiFi->_wiFiSTAEventGroup, WIFI_STA_FAILED_BIT);
             for (const auto& handler : wiFi->_onConnectionFailedHandlers) {
                 handler();
             }
         }
-        else if (wiFi->_manuallyDisconnect) {
-            wiFi->_manuallyDisconnect = false;
+        else if (wiFi->_isManuallyDisconnect) {
+            wiFi->_isManuallyDisconnect = false;
             for (const auto& handler : wiFi->_onDisconnectedHandlers) {
                 handler();
             }
@@ -76,13 +84,23 @@ void WiFi::wiFiEventsHandler(void* arguments, esp_event_base_t eventBase, const 
         }
     }
     else if (eventId == WIFI_EVENT_AP_START) {
-        for (const auto& handler : wiFi->_onAPStartedHandlers) {
-            handler(reinterpret_cast<const char*>(wiFi->_apConfig.ap.ssid), reinterpret_cast<const char*>(wiFi->_apConfig.ap.password));
+         if (!wiFi->_isAPStarted && wiFi->_isAPStarting) {
+            wiFi->_isAPStarted = true;
+            wiFi->_isAPStarting = false;
+            for (const auto& handler : wiFi->_onAPStartedHandlers) {
+                handler(reinterpret_cast<const char*>(wiFi->_apConfig.ap.ssid), reinterpret_cast<const char*>(wiFi->_apConfig.ap.password));
+            }
         }
     }
     else if (eventId == WIFI_EVENT_AP_STOP) {
-        for (const auto& handler : wiFi->_onAPStoppedHandlers) {
-            handler(reinterpret_cast<const char*>(wiFi->_apConfig.ap.ssid), reinterpret_cast<const char*>(wiFi->_apConfig.ap.password));
+        if (wiFi->_isSwitchingMode) {
+            wiFi->_isSwitchingMode = false;
+        }
+        else if (wiFi->_isAPStarted) {
+            wiFi->_isAPStarted = false;
+            for (const auto& handler : wiFi->_onAPStoppedHandlers) {
+                handler(reinterpret_cast<const char*>(wiFi->_apConfig.ap.ssid), reinterpret_cast<const char*>(wiFi->_apConfig.ap.password));
+            }
         }
     }
     else if (eventId == WIFI_EVENT_AP_STADISCONNECTED) {
@@ -95,26 +113,239 @@ void WiFi::wiFiEventsHandler(void* arguments, esp_event_base_t eventBase, const 
     }
 }
 
+void WiFi::connect(const std::string& ssid, const std::string& password)
+{
+    if (_isConnected || _isConnecting) {
+        return;
+    }
+    if (_netifSta != nullptr) {
+        esp_netif_destroy(_netifSta);
+    }
+    _netifSta = esp_netif_create_default_wifi_sta();
+    _staConfig.sta.failure_retry_cnt = 0;
+    strncpy(reinterpret_cast<char*>(_staConfig.sta.ssid), ssid.c_str(), sizeof(_staConfig.sta.ssid));
+    if (!password.empty()) {
+        strncpy(reinterpret_cast<char*>(_staConfig.sta.password), password.c_str(), sizeof(_staConfig.sta.password));
+    }
+    _isConnecting = true;
+    for (const auto& handler : _onConnectingHandlers) {
+        handler();
+    }
+    wifi_mode_t mode;
+    esp_wifi_get_mode(&mode);
+    if (mode == WIFI_MODE_AP) {
+        _isSwitchingMode = true;
+        esp_wifi_set_mode(WIFI_MODE_APSTA);
+    }
+    else {
+        esp_wifi_set_mode(WIFI_MODE_STA);
+    }
+    esp_wifi_set_config(WIFI_IF_STA, &_staConfig);
+    esp_wifi_start();
+    if (!_wiFiSTAEventGroup) {
+        _wiFiSTAEventGroup = xEventGroupCreate();
+    }
+    xEventGroupWaitBits(_wiFiSTAEventGroup, WIFI_STA_CONNECTED_BIT | WIFI_STA_FAILED_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+}
+
 void WiFi::startAP(const std::string& ssid, const std::string& password)
 {
-    init();
-    esp_netif_create_default_wifi_ap();
+    if (_isAPStarted || _isAPStarting) {
+        return;
+    }
+    if (_netifAP != nullptr) {
+        esp_netif_destroy(_netifAP);
+    }
+    _netifAP = esp_netif_create_default_wifi_ap();
     _apConfig.ap.max_connection = 1;
     strncpy(reinterpret_cast<char*>(_apConfig.ap.ssid), ssid.c_str(), sizeof(_apConfig.ap.ssid));
-    if (!password.empty()) {
+    if (password.empty()) {
+        _apConfig.ap.authmode = WIFI_AUTH_OPEN;
+    }
+    else {
         _apConfig.ap.authmode = WIFI_AUTH_WPA2_PSK;
         strncpy(reinterpret_cast<char*>(_apConfig.ap.password), password.c_str(), sizeof(_apConfig.ap.password));
     }
-    esp_wifi_set_mode(WIFI_MODE_AP);
+    _isAPStarting = true;
+    for (const auto& handler : _onAPStartingHandlers) {
+        handler(reinterpret_cast<const char*>(_apConfig.ap.ssid), reinterpret_cast<const char*>(_apConfig.ap.password));
+    }
+    wifi_mode_t mode;
+    esp_wifi_get_mode(&mode);
+    if (mode == WIFI_MODE_STA) {
+        _isSwitchingMode = true;
+        esp_wifi_set_mode(WIFI_MODE_APSTA);
+    }
+    else {
+        esp_wifi_set_mode(WIFI_MODE_AP);
+    }
     esp_wifi_set_config(WIFI_IF_AP, &_apConfig);
-    esp_wifi_set_storage(WIFI_STORAGE_RAM);
+    esp_wifi_start();
+}
+
+void WiFi::reconnect() {
+    if (_netifSta == nullptr || _isConnected || _isConnecting) {
+        return;
+    }
+    _isConnecting = true;
+    for (const auto& handler : _onConnectingHandlers) {
+        handler();
+    }
+    wifi_mode_t mode;
+    esp_wifi_get_mode(&mode);
+    if (mode == WIFI_MODE_AP) {
+        _isSwitchingMode = true;
+        esp_wifi_set_mode(WIFI_MODE_APSTA);
+    }
+    else {
+        esp_wifi_set_mode(WIFI_MODE_STA);
+    }
+    esp_wifi_start();
+}
+
+void WiFi::disconnect() {
+    if (!_isConnected) {
+        return;
+    }
+    _isConnected = false;
+    _isManuallyDisconnect = true;
+    wifi_mode_t mode;
+    esp_wifi_get_mode(&mode);
+    if (mode == WIFI_MODE_APSTA) {
+        _isSwitchingMode = true;
+        for (const auto& handler : _onDisconnectedHandlers) {
+            handler();
+        }
+        esp_wifi_set_mode(WIFI_MODE_AP);
+    }
+    else if (mode == WIFI_MODE_STA) {
+        esp_wifi_disconnect();
+        esp_wifi_stop();
+        esp_wifi_set_mode(WIFI_MODE_NULL);
+    }
+}
+
+void WiFi::restartAP()
+{
+    if (_netifAP == nullptr || _isAPStarted || _isAPStarting) {
+        return;
+    }
+    _isAPStarting = true;
+    for (const auto& handler : _onAPStartingHandlers) {
+        handler(reinterpret_cast<const char*>(_apConfig.ap.ssid), reinterpret_cast<const char*>(_apConfig.ap.password));
+    }
+    wifi_mode_t mode;
+    esp_wifi_get_mode(&mode);
+    if (mode == WIFI_MODE_STA) {
+        _isSwitchingMode = true;
+        esp_wifi_set_mode(WIFI_MODE_APSTA);
+    }
+    else {
+        esp_wifi_set_mode(WIFI_MODE_AP);
+    }
     esp_wifi_start();
 }
 
 void WiFi::stopAP()
 {
-    esp_wifi_stop();
-    esp_wifi_set_mode(WIFI_MODE_STA);
+    if (!_isAPStarted) {
+        return;
+    }
+    _isAPStarted = false;
+    wifi_mode_t mode;
+    esp_wifi_get_mode(&mode);
+    if (mode == WIFI_MODE_APSTA) {
+        _isSwitchingMode = true;
+        for (const auto& handler : _onAPStoppedHandlers) {
+            handler(reinterpret_cast<const char*>(_apConfig.ap.ssid), reinterpret_cast<const char*>(_apConfig.ap.password));
+        }
+        esp_wifi_set_mode(WIFI_MODE_STA);
+    }
+    else if (mode == WIFI_MODE_AP) {
+        esp_wifi_stop();
+        esp_wifi_set_mode(WIFI_MODE_NULL);
+    }
+}
+
+std::vector<WiFi::AccessPoint> WiFi::listAccessPoints() const
+{
+    std::vector<AccessPoint> accessPoints;
+    if (_netifSta == nullptr) {
+        return accessPoints;
+    }
+    esp_wifi_scan_start(nullptr, true);
+    uint16_t apNum = 0;
+    esp_wifi_scan_get_ap_num(&apNum);
+    if (apNum > 0) {
+        auto* apRecords = static_cast<wifi_ap_record_t *>(malloc(apNum * sizeof(wifi_ap_record_t)));
+        memset(apRecords, 0, apNum * sizeof(wifi_ap_record_t));
+        esp_wifi_scan_get_ap_records(&apNum, apRecords);
+        for (int i = 0; i < apNum; i++) {
+            accessPoints.emplace_back(AccessPoint(reinterpret_cast<const char*>(apRecords[i].ssid), apRecords[i].rssi));
+        }
+        free(apRecords);
+    }
+    return accessPoints;
+}
+
+std::vector<WiFi::Client> WiFi::listClients() const
+{
+    std::vector<Client> clients;
+    if (_netifSta == nullptr) {
+        return clients;
+    }
+    wifi_sta_list_t staList;
+    esp_wifi_ap_get_sta_list(&staList);
+    for (int i = 0; i < staList.num; i++) {
+        char macAddress[18];
+        snprintf(macAddress, sizeof(macAddress), "%02X:%02X:%02X:%02X:%02X:%02X", staList.sta[i].mac[0], staList.sta[i].mac[1], staList.sta[i].mac[2], staList.sta[i].mac[3], staList.sta[i].mac[4], staList.sta[i].mac[5]);
+        clients.emplace_back(Client(macAddress, staList.sta[i].rssi));
+    }
+    return clients;
+}
+
+bool WiFi::isConnected() const
+{
+    return _isConnected;
+}
+
+bool WiFi::isAPActive() const
+{
+    return _isAPStarted;
+}
+
+std::string WiFi::getAPIP() const
+{
+    if (_netifAP == nullptr) {
+        return "";
+    }
+    esp_netif_ip_info_t ipInfo;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ipInfo);
+    char ip[16];
+    snprintf(ip, sizeof(ip), IPSTR, IP2STR(&ipInfo.ip));
+    return ip;
+}
+
+int WiFi::getRSSI() const
+{
+    if (_netifSta == nullptr) {
+        return 0;
+    }
+    int rssi;
+    esp_wifi_sta_get_rssi(&rssi);
+    return rssi;
+}
+
+std::string WiFi::getMAC() const
+{
+    if (_netifSta == nullptr) {
+        return "";
+    }
+    uint8_t mac[6];
+    esp_wifi_get_mac(WIFI_IF_STA, mac);
+    char macAddress[18];
+    snprintf(macAddress, sizeof(macAddress), "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return macAddress;
 }
 
 void WiFi::onConnecting(const std::function<void()>& handler)
@@ -142,6 +373,11 @@ void WiFi::onDisconnected(const std::function<void()>& handler)
     _onDisconnectedHandlers.push_back(handler);
 }
 
+void WiFi::onAPStarting(const std::function<void(const std::string& ssid, const std::string& password)>& handler)
+{
+    _onAPStartingHandlers.push_back(handler);
+}
+
 void WiFi::onAPStarted(const std::function<void(const std::string& ssid, const std::string& password)>& handler)
 {
     _onAPStartedHandlers.push_back(handler);
@@ -162,75 +398,28 @@ void WiFi::onAPClientDisconnected(const std::function<void(const std::string& cl
     _onAPClientDisconnectedHandlers.push_back(handler);
 }
 
-bool WiFi::isConnected() const
+WiFi::AccessPoint::AccessPoint(std::string ssid, const int rssi)
+    : _ssid(std::move(ssid)), _rssi(rssi) {}
+
+const std::string& WiFi::AccessPoint::getSSID() const
 {
-    return _isConnected;
+    return _ssid;
 }
 
-void WiFi::connect(const std::string& ssid, const std::string& password)
+int WiFi::AccessPoint::getRSSI() const
 {
-    if (_isConnected || _isConnecting) {
-        return;
-    }
-    init();
-    esp_netif_create_default_wifi_sta();
-    strncpy(reinterpret_cast<char*>(_staConfig.sta.ssid), ssid.c_str(), sizeof(_staConfig.sta.ssid));
-    if (!password.empty()) {
-        strncpy(reinterpret_cast<char*>(_staConfig.sta.password), password.c_str(), sizeof(_staConfig.sta.password));
-    }
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(WIFI_IF_STA, &_staConfig);
-    esp_wifi_set_storage(WIFI_STORAGE_RAM);
-    esp_wifi_start();
-    _isConnecting = true;
-    for (const auto& handler : _onConnectingHandlers) {
-        handler();
-    }
-    _wiFiSTAEventGroup = xEventGroupCreate();
-    xEventGroupWaitBits(_wiFiSTAEventGroup, WIFI_STA_CONNECTED_BIT | WIFI_STA_FAILED_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+    return _rssi;
 }
 
-void WiFi::reconnect() {
-    if (_isConnected || _isConnecting) {
-        return;
-    }
-    esp_wifi_connect();
-    _isConnecting = true;
-    for (const auto& handler : _onConnectingHandlers) {
-        handler();
-    }
-}
+WiFi::Client::Client(std::string  mac, const int rssi)
+    : _mac(std::move(mac)), _rssi(rssi) {}
 
-void WiFi::disconnect() {
-    if (!_isConnected) {
-        return;
-    }
-    esp_wifi_disconnect();
-    _isConnected = false;
-    _manuallyDisconnect = true;
-}
-
-std::string WiFi::getAPIP()
+const std::string& WiFi::Client::getMAC() const
 {
-    esp_netif_ip_info_t ipInfo;
-    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ipInfo);
-    char ip[16];
-    snprintf(ip, sizeof(ip), IPSTR, IP2STR(&ipInfo.ip));
-    return ip;
+    return _mac;
 }
 
-int WiFi::getRSSI()
+int WiFi::Client::getRSSI() const
 {
-    wifi_ap_record_t apInfo;
-    esp_wifi_sta_get_ap_info(&apInfo);
-    return apInfo.rssi;
-}
-
-std::string WiFi::getMAC()
-{
-    uint8_t mac[6];
-    esp_wifi_get_mac(WIFI_IF_STA, mac);
-    char macAddress[18];
-    snprintf(macAddress, sizeof(macAddress), "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-    return macAddress;
+    return _rssi;
 }
--- WiFi/WiFi.h
+++ WiFi/WiFi.h
@@ -18,44 +18,81 @@
 
 class WiFi final {
     public:
+        class AccessPoint final {
+            friend class WiFi;
+
+            public:
+                [[nodiscard]] const std::string& getSSID() const;
+                [[nodiscard]] int getRSSI() const;
+
+            private:
+                AccessPoint(std::string ssid, int rssi);
+                std::string _ssid;
+                int _rssi;
+        };
+
+        class Client final {
+            friend class WiFi;
+
+            public:
+                [[nodiscard]] const std::string& getMAC() const;
+                [[nodiscard]] int getRSSI() const;
+
+            private:
+                Client(std::string  mac, int rssi);
+
+            std::string _mac;
+                int _rssi;
+        };
+
         WiFi();
         ~WiFi();
         void connect(const std::string& ssid, const std::string& password = "");
+        void startAP(const std::string& ssid, const std::string& password = "");
         void reconnect();
         void disconnect();
+        void restartAP();
+        void stopAP();
+        [[nodiscard]] std::vector<AccessPoint> listAccessPoints() const;
+        [[nodiscard]] std::vector<Client> listClients() const;
+        [[nodiscard]] bool isConnected() const;
+        [[nodiscard]] bool isAPActive() const;
+        [[nodiscard]] std::string getAPIP() const;
+        [[nodiscard]] int getRSSI() const;
+        [[nodiscard]] std::string getMAC() const;
         void onConnecting(const std::function<void()>& handler);
         void onConnected(const std::function<void(const std::string& ip)>& handler);
         void onConnectionLost(const std::function<void()>& handler);
         void onConnectionFailed(const std::function<void()>& handler);
         void onDisconnected(const std::function<void()>& handler);
+        void onAPStarting(const std::function<void(const std::string& ssid, const std::string& password)>& handler);
         void onAPStarted(const std::function<void(const std::string& ssid, const std::string& password)>& handler);
         void onAPStopped(const std::function<void(const std::string& ssid, const std::string& password)>& handler);
         void onAPClientConnected(const std::function<void(const std::string& clientIp, const std::string& clientMac)>& handler);
         void onAPClientDisconnected(const std::function<void(const std::string& clientMac)>& handler);
-        [[nodiscard]] bool isConnected() const;
-        void startAP(const std::string& ssid, const std::string& password = "");
-        static void stopAP();
-        static std::string getAPIP();
-        static int getRSSI();
-        static std::string getMAC();
 
     private:
         wifi_config_t _apConfig;
         wifi_config_t _staConfig;
+        esp_netif_t* _netifAP;
+        esp_netif_t* _netifSta;
         bool _isConnected;
         bool _isConnecting;
-        bool _manuallyDisconnect;
+        bool _isManuallyDisconnect;
+        bool _isAPStarted;
+        bool _isAPStarting;
+        bool _isSwitchingMode;
         EventGroupHandle_t _wiFiSTAEventGroup;
         std::vector<std::function<void()>> _onConnectingHandlers;
         std::vector<std::function<void(const std::string& ip)>> _onConnectedHandlers;
         std::vector<std::function<void()>> _onConnectionLostHandlers;
         std::vector<std::function<void()>> _onConnectionFailedHandlers;
         std::vector<std::function<void()>> _onDisconnectedHandlers;
+        std::vector<std::function<void(const std::string& ssid, const std::string& password)>> _onAPStartingHandlers;
         std::vector<std::function<void(const std::string& ssid, const std::string& password)>> _onAPStartedHandlers;
         std::vector<std::function<void(const std::string& ssid, const std::string& password)>> _onAPStoppedHandlers;
         std::vector<std::function<void(const std::string& clientIp, const std::string& clientMac)>> _onAPClientConnectedHandlers;
         std::vector<std::function<void(const std::string& clientMac)>> _onAPClientDisconnectedHandlers;
-        void init();
         static void ipEventsHandler(void* arguments, esp_event_base_t eventBase, int32_t eventId, void* eventData);
         static void wiFiEventsHandler(void* arguments, esp_event_base_t eventBase, int32_t eventId, void* eventData);
 };
